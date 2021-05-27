7bbd8657573fb79f089144ff19504b5e51de30e9
louise hill <louisehilldesigner@gmail.com>
2021-05-28 00:52:20 +0100

    validations complete

---
+++ db/migrate/20210527115557_create_movies.rb
@@ -0,0 +1,12 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :title, null: false
+      t.text :overview
+      t.string :poster_url
+      t.float :rating, default: 0.0
+
+      t.timestamps
+    end
+  end
+end
